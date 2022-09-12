8aaa58d647cc819022d1b62137d3030cd608e9a2
matydrums <matolmd@gmail.com>
2022-09-12 14:37:44 -0300

    Add existing file

---
+++ index.js
@@ -0,0 +1,37 @@
+alert('Hello world!!!')
+ 
+//futas
+var frutas = ("Naranjas")
+var frutas = ("Bananas")
+var frutas = ("Peras")
+var frutas = ("Manzanas")
+
+function comprar(frutas) {
+console.log("gracias por tu compra, has comprado " + frutas);
+alert("gracias por tu compra, has comprado " + frutas)
+}
+
+//VERDURAS
+var verduras = ("Papas")
+var verduras = ("Apio")
+var verduras = ("Lechuga")
+var verduras = ("Espinacas")
+
+function comprar(verduras) {
+console.log("gracias por tu compra, has comprado " + verduras);
+alert("gracias por tu compra, has comprado " + verduras);
+}
+
+//PROMOCIONES 
+
+
+var promocion1 = ("2k de Papas + 1k de Manzanas + 1k de verdura a Eleccion")
+var promocion2 = ("1k Papas + 1/2k de bananas, + 1k de Naranja")
+var promocion3 = ("1k de Mandarina + 2k de Acelga + 1k de Tomates")
+var promocion4 = ("1k de Espinacas + 1k de Papas + 1k de fruta a eleccion")
+
+function comprar(promocion1, promocion2, promocion3, promocion4) 
+{
+    console.log("gracias por tu compra, has comprado " + promocion1, promocion2, promocion3, promocion4);
+    alert("gracias por tu compra, has comprado " + promocion1, promocion2, promocion3, promocion4);
+}
